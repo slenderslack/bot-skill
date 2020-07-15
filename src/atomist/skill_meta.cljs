c95c56c45067df91e3021eb39b7b8b022edf2e46
Jim Clark <slimslenderslacks@gmail.com>
2020-07-14 17:49:10 -0700

    Bump bot-skill to 0.1.4 and spit metadata

--- src/atomist/skill_meta.cljs
+++ src/atomist/skill_meta.cljs
@@ -3,6 +3,6 @@
 
 (def module-name "bot-skill")
 (def path ".")
-(def version "0.1.3")
-(def tag "0.1.3")
-(def generated-at "2020-07-15T00:25:23Z")
+(def version "0.1.4")
+(def tag "0.1.4")
+(def generated-at "2020-07-15T00:49:10Z")
